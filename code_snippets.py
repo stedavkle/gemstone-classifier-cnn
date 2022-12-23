40f5dd4efde7d789c9c8825ed0d7e14f3add0f0a
stedavkle <77785743+stedavkle@users.noreply.github.com>
2022-12-23 11:06:05 +0100

    added snippets

--- code_snippets.py
+++ code_snippets.py
@@ -6,16 +6,31 @@ for root, dirs, files in os.walk('/home/david/projects/Gemstones-Convolutional-N
         if 'flip' in file or 'rot' in file:
             os.remove(os.path.join(root, file))
 # %%
-# remove images with less than 1000 pictures
+# remove folders with less than 0.75*goal amount of pictures
 import os
+goal_num = 2000
 for root, dirs, files in os.walk('/home/david/projects/Gemstones-Convolutional-Neural-Network/1_Fetch_data/data/gempundit_nohands_crop_1k'):
-    if len(files) < 600:
+    if len(files) < 0.75*goal_num:
         for file in files:
             os.remove(os.path.join(root, file))
-# %%
-# remove empty folders
-import os
-for root, dirs, files in os.walk('/home/david/projects/Gemstones-Convolutional-Neural-Network/1_Fetch_data/data/gempundit_nohands_crop_1k'):
-    if len(files) == 0:
         os.rmdir(root)
 # %%
+## delete files wich are not an image
+import os
+import imghdr
+for root, dirs, files in os.walk("./"):
+    for file in files:
+        if not imghdr.what(root+"/"+file) == 'jpeg':
+            os.remove(root+"/"+file)
+            print(root+"/"+file)
+# %%
+# make sure in no folder are miore than 1000 images
+import os
+import numpy as np
+goal_num = 2000
+for root, dirs, files in os.walk("C:/Users/david/Documents/projects/Gemstone_CNN/data0/data/gempundit_nohands_crop_1k"):
+    if len(files) > goal_num:
+        unnecerary_pictures = np.random.choice(files, len(files)-goal_num, replace=False)
+        print(len(unnecerary_pictures))
+        for file in unnecerary_pictures:
+            os.remove(root+"/"+file)
