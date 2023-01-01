4212526e39f79abc4a9159b703f7d4ace11f7d31
stedavkle <77785743+stedavkle@users.noreply.github.com>
2023-01-01 16:55:43 +0100

    added helpfull code snippets

--- code_snippets.py
+++ code_snippets.py
@@ -1,7 +1,10 @@
 # %%
+root_dir = r'C:\Users\david\Documents\projects\gemstone-classifier-cnn\data\gempundit_nohands_crop_2k'
+
+
 # remove transformed images
 import os
-for root, dirs, files in os.walk('/home/david/projects/Gemstones-Convolutional-Neural-Network/1_Fetch_data/data/gempundit_nohands_crop_1k'):
+for root, dirs, files in os.walk(root_dir):
     for file in files:
         if 'flip' in file or 'rot' in file:
             os.remove(os.path.join(root, file))
@@ -9,28 +12,52 @@ for root, dirs, files in os.walk('/home/david/projects/Gemstones-Convolutional-N
 # remove folders with less than 0.75*goal amount of pictures
 import os
 goal_num = 2000
-for root, dirs, files in os.walk('/home/david/projects/Gemstones-Convolutional-Neural-Network/1_Fetch_data/data/gempundit_nohands_crop_1k'):
+for root, dirs, files in os.walk(root_dir):
     if len(files) < 0.75*goal_num:
+        print(root.split('\\')[-1])
         for file in files:
             os.remove(os.path.join(root, file))
-        os.rmdir(root)
+        try:
+            os.rmdir(root)
+        except Exception as e:
+            print(e)
 # %%
 ## delete files wich are not an image
 import os
 import imghdr
-for root, dirs, files in os.walk("./"):
+for root, dirs, files in os.walk(root_dir):
     for file in files:
         if not imghdr.what(root+"/"+file) == 'jpeg':
             os.remove(root+"/"+file)
             print(root+"/"+file)
 # %%
+# convert all webp to jpeg
+import os
+import imghdr
+from PIL import Image
+for root, dirs, files in os.walk(root_dir):
+    for file in files:
+        if imghdr.what(root+"/"+file) == 'webp':
+            im = Image.open(root+"/"+file).convert("RGB")
+            im.save(root+"/"+file, "jpeg")
+            print(root+"/"+file)
+# list all files not an jpeg
+import os
+import imghdr
+for root, dirs, files in os.walk(root_dir):
+    for file in files:
+        if not imghdr.what(root+"/"+file) == 'jpeg':
+            print(root+"/"+file)
+            print(imghdr.what(root+"/"+file))
+
+# %%
 # make sure in no folder are miore than 1000 images
 import os
 import numpy as np
 goal_num = 2000
-for root, dirs, files in os.walk("C:/Users/david/Documents/projects/Gemstone_CNN/data0/data/gempundit_nohands_crop_1k"):
+for root, dirs, files in os.walk(root_dir):
     if len(files) > goal_num:
         unnecerary_pictures = np.random.choice(files, len(files)-goal_num, replace=False)
-        print(len(unnecerary_pictures))
+        print(root.split('\\')[-1], len(unnecerary_pictures))
         for file in unnecerary_pictures:
             os.remove(root+"/"+file)
